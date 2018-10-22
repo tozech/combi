d38a9e116dc9227c587f47b710d0130dc36982f0
tozech <tozech@gmail.com>
2018-10-22 22:47:44 +0200

    Enh: Rank histogram of 12h values

--- forecast.py
+++ forecast.py
@@ -27,7 +27,8 @@ dwd_meta = pd.read_csv(metadata, sep=';')
 #%%
 altitude, latitude, longitude = tuple(dwd_meta[['Stationshoehe', 'Geogr.Breite', 'Geogr.Laenge']].iloc[-1].values)
 #%%
-ds['GHI'] = ds['ssrd'].diff('step')
+ds['GHI_fc'] = ds['ssrd'].diff('step')
+ds['GHI_fc'] = ds['GHI_fc'] / 3600 / 12 #Convert to Wh/m^2
 #%%
 ds_dayahead = ds.sel(step=slice(pd.Timedelta('23:59:59'), pd.Timedelta('1d 23:59:59')))
 #%%
@@ -36,14 +37,31 @@ ds_station = ds_dayahead.sel(latitude=latitude, longitude=longitude, method='nea
 df = ds_station.to_dataframe()
 df = df.reset_index()
 df['valid_time'] = df['time'] + df['step']
+df = df.set_index('valid_time')
 #%%
 dwd = dwd.rename(columns={'GS_10': 'GHI_meas'})
+#%%
+dwd['GHI_meas'] = dwd['GHI_meas'] / 3600 * 100**2 * 6
 dwd_12h = dwd.resample('12h', closed='right', label='right').mean()
 #%%
 dwd_12h_centered = dwd_12h.copy(deep=True)
 dwd_12h_centered.index = dwd_12h_centered.index - pd.Timedelta('6h')
+df_centered = df.copy(deep=True)
+df_centered.index = df_centered.index - pd.Timedelta('6h')
 #%%
-start, end = '2017-07-01', '2017-07-04'
+start, end = '2017-07-02', '2017-07-31'
 fig, ax = plt.subplots()
 dwd.loc[start:end, 'GHI_meas'].plot(ax=ax)
 dwd_12h_centered.loc[start:end, 'GHI_meas'].plot(ax=ax)
+df_centered.loc[start:end, 'GHI_fc'].plot(ax=ax, lw=0, marker='.')
+
+meas_ts = dwd_12h.loc[start:end, 'GHI_meas']
+fc_ts = df.loc[start:end, ['GHI_fc', 'number']]
+#%%
+fc = fc_ts.reset_index()
+fc = fc.pivot(index='valid_time', columns='number', values='GHI_fc')
+#%%
+fig, ax = plt.subplots()
+mask = [True for x in meas_ts.values]
+rh, bins = ranky.rankz(meas_ts.values, fc.values.T, mask)
+ax.bar(bins[:-1], rh)
