beb73888bdf4c27021b1dab159dfe07a5e7e163c
tozech <tozech@gmail.com>
2019-05-23 14:05:08 +0200

    Enh: simple analog ensemble

--- analog_ensemble.py
+++ analog_ensemble.py
@@ -6,13 +6,16 @@ Created on Thu May 23 10:20:38 2019
 @author: tzech
 """
 from collections import OrderedDict
+import gc
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import xarray as xr
 
-import seaborn as sns   
+import seaborn as sns  
+
+import ranky 
 #%%
 fname = '/home/tzech/ownCloud/Data/combi/cmv_nwp/Q1/20160401-20160430.nc'
 cmv_nwp = xr.open_dataset(fname)
@@ -34,6 +37,14 @@ df_meas = df_meas.loc[:, 'measurements']
 df_meas = df_meas.reset_index()
 #%%
 df = df_cmv_nwp.merge(df_meas, on='valid_time')
+#%%
+cmv_nwp.close()
+del(cmv_nwp)
+meas.close()
+del(meas)
+del(df_meas)
+del(df_cmv_nwp)
+gc.collect()
 #%% Add time of day
 df['valid_time_of_day'] = df.valid_time.dt.time
 #%%
@@ -55,17 +66,21 @@ plt.ion()
 #%% Leave one day out cross validation
 # Start with leave 2016-04-30 out and use 15min step only
 test_date = pd.Timestamp('2016-04-30')
-fil_test = df['base_time'] >= test_date
-fil_train = df['base_time'] < test_date
+fil_test = df['base_time'].dt.date == test_date.date()
+fil_train = df['base_time'].dt.date != test_date.date()
 df_test = df.loc[fil_test]
 df_train = df.loc[fil_train]
 #%% Build persistence ensemble for same datetime
-def latest_samples(data, n=10):
-    data = data.sort_values('base_time')
+def get_samples(data, key, n=10, ascending=True):
+    data = data.sort_values(key, ascending=ascending)
     return data.iloc[-n:, :]
 
+def latest_samples(data, n=10):
+    return get_samples(data, 'base_time')
+
+
 def latest_measurements(data, n=10):
-    latest = latest_samples(data)
+    latest = latest_samples(data, n)
     latest = latest.reset_index()
     meas = latest['measurements']
     return meas
@@ -75,6 +90,9 @@ def sort_ensemble(data):
     values.sort(axis=1)
     return pd.DataFrame(values, index=data.index, columns=data.columns)
 
+def time_of_day(time_col):
+    return time_col.apply(lambda t: pd.Timedelta(t.strftime('%H:%M:%S')))
+
 fil_now = df['step'] == pd.Timedelta('0min')
 grps_valid_time_of_day = df_train.loc[fil_now, :].groupby('valid_time_of_day')
 grps_valid_time_of_day[['base_time', 'forecast', 'measurements']].last()
@@ -82,13 +100,55 @@ pers_list = [(k, latest_measurements(grp)) for k, grp in grps_valid_time_of_day]
 pers = pd.DataFrame(OrderedDict(pers_list))
 pers = pers.transpose()
 pers = pers.reset_index()
-pers['time_of_day'] = pers['index'].apply(lambda t: pd.Timedelta(t.strftime('%H:%M:%S')))
+pers['time_of_day'] = time_of_day(pers['index'])
 pers['valid_time'] = pers['time_of_day'] + test_date
 pers = pers[[col for col in pers.columns if col not in ['index', 'time_of_day']]]
 pers = pers.set_index('valid_time')
 pers = sort_ensemble(pers)
 #%%
+meas_ts = df_test.loc[fil_now, :].set_index('valid_time')['measurements']
+meas_time_of_day = time_of_day(meas_ts.reset_index()['valid_time'])
+current_fc = df_test.loc[fil_step, :].set_index('valid_time')['forecast']
 fig, ax = plt.subplots()
 pers.plot(ax=ax, color='k', alpha=0.5)
-df_test.loc[fil_now, :].set_index('valid_time')['measurements'].plot(ax=ax, lw=2)
+meas_ts.plot(ax=ax, lw=2)
+current_fc.plot(ax=ax, lw=2)
+#%%
+fig, ax = plt.subplots()
+mask = [True for x in meas_ts.values]
+rh, bins = ranky.rankz(meas_ts.values, pers.values.T, mask)
+ax.bar(bins[:-1], rh)
+#%% Analog ensemble only on current values
+df_train_test = df_train.merge(df_test, on='valid_time_of_day', suffixes=('', '_test'))
+#%%
+df_train_test['abs_train_test'] = (df_train_test['forecast'] - df_train_test['forecast_test']).abs()
+#%%
+def analogs_one_val(data, key='abs_train_test', n=10):
+    analogs = get_samples(data, key, n, ascending=False)
+    analogs = analogs.reset_index()
+    meas = analogs['measurements']
+    return meas
 
+grps_valid_time_of_day = df_train_test.groupby('valid_time_of_day')
+analogs_list = [(k, analogs_one_val(grp)) for k, grp in grps_valid_time_of_day]
+analog = pd.DataFrame(OrderedDict(analogs_list))
+analog = analog.transpose()
+analog = analog.reset_index()
+analog['time_of_day'] = analog['index'].apply(lambda t: pd.Timedelta(t.strftime('%H:%M:%S')))
+analog['valid_time'] = analog['time_of_day'] + test_date
+analog = analog[[col for col in analog.columns if col not in ['index', 'time_of_day']]]
+analog = analog.set_index('valid_time')
+analog = sort_ensemble(analog)
+#%%
+fil_times = [t + test_date for t in time_of_day(analog.reset_index()['valid_time']) if t in list(meas_time_of_day)]
+analog = analog.loc[fil_times, :]
+#%%
+fig, ax = plt.subplots()
+analog.plot(ax=ax, color='k', alpha=0.5)
+df_test.loc[fil_now, :].set_index('valid_time')['measurements'].plot(ax=ax, lw=2)
+df_test.loc[fil_step, :].set_index('valid_time')['forecast'].plot(ax=ax, lw=2)
+#%%
+fig, ax = plt.subplots()
+mask = [True for x in meas_ts.values]
+rh, bins = ranky.rankz(meas_ts.values, analog.values.T, mask)
+ax.bar(bins[:-1], rh)
