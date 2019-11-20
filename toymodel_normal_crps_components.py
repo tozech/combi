b7d2d2822dcd01e0229c918f60bfab7e89f1908f
tozech <tozech@gmail.com>
2019-11-20 14:04:53 +0100

    Enh: Run toymodel for different n_ens

--- toymodel_normal_crps_components.py
+++ toymodel_normal_crps_components.py
@@ -15,13 +15,13 @@ from properscoring import mean_crps, crps_ensemble, crps_gaussian
 #%%
 df = pd.DataFrame(columns=['n_ens', 'm_times', 'mean_crps_dist', 'mean_crps_ens', 'rel', 'res', 'unc'])
 #%%
-n_ens = 50
+n_ens = 100
 for exp_m_times in range(2, 7):
     m_times = 10**exp_m_times
     print('exp_m_times', exp_m_times, 'm_times', m_times)
     ens = np.random.randn(m_times, n_ens)
     obs = np.random.randn(m_times)
-    
+
     crps_dist = crps_gaussian(obs, 0, 1)
     mean_crps_dist = np.mean(crps_dist)
     crps_values = crps_ensemble(obs, ens)
@@ -35,5 +35,20 @@ for exp_m_times in range(2, 7):
                     'res': res,
                     'unc': unc},
                     ignore_index=True)
-    
 
+#%% unc component converges towards CRPS dist
+print(df['mean_crps_dist'] - df['unc'])
+#%% rel component of ensemble converges for a fixed n_ens with larger m_times
+# to a finite number and therefore the
+# difference between mean_crps_dist and mean_crps_ens
+fname = 'crps_components_m_times_100-1M.csv'
+df_store = pd.read_csv(fname, index_col=0)
+save = False
+if save:
+    df_store = pd.concat([df_store, df])
+    df_store = df_store.sort_values(['n_ens', 'm_times'])
+    df_store.to_csv(fname)
+#%% rel component converges to zero with larger n_ens for large m_times
+# Hence, finite reliability here is only a finite size effect of the ensemble
+df_store_max = df_store[df_store.m_times == 10**6]
+plt.plot(df_store_max.n_ens, df_store_max.rel)
