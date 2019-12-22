f43b589efc25ec235e49cf613802b16496623de7
tozech <tozech@gmail.com>
2019-12-22 17:49:01 +0100

    Enh: For new input with MAE and RMSE

--- analysis_mean_crps_vd_naive.py
+++ analysis_mean_crps_vd_naive.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-datadir = '/home/tzech/ownCloud/Results/time_corr/'
+datadir = '/home/tzech/ownCloud/Results/time_corr/run_2019-12-22'
 methods = ['vd_naive']
 calib_freqs = ['15min', '3h']
 issorteds = [False]
@@ -35,6 +35,8 @@ def read_single_file(datadir, method, calib_freq, issorted, month):
     df = df.set_index('winsize')
     for c in ['mean_crps', 'reliability', 'resolution']:
         df['{0}_by_unc'.format(c)] = df[c] / df['uncertainty']
+    for c in ['mean_crps', 'reliability', 'resolution', 'uncertainty']:
+        df['{0}_by_mae'.format(c)] = df[c] / df['mae']
     df = df.stack()
     df = df.reset_index()
     df.columns = ['winsize', 'metric', 'value']
@@ -52,9 +54,25 @@ for method in methods:
                 tdf = read_single_file(datadir, method, calib_freq, issorted, month)
                 df_list.append(tdf)
 
+df_list.append(read_single_file(datadir, 'uncalib', '15min', False, '2018-01'))
 df_list.append(read_single_file(datadir, 'uncalib', '15min', True, '2018-01'))
+df_list.append(read_single_file(datadir, 'uncalib', '15min', False, '2018-07'))
+df_list.append(read_single_file(datadir, 'uncalib', '15min', True, '2018-07'))
+
+df_list.append(read_single_file(datadir, 'vd_naive', '15min', True, '2018-01'))
+df_list.append(read_single_file(datadir, 'vd_naive', '15min', True, '2018-07'))
                 
 df = pd.concat(df_list)
+assert False
+#%% Add combined method and calib_freq
+df['method_freq'] = df.apply(lambda row: "{0}_{1}".format(row['method'], row['calib_freq']), axis=1)
+#%%
+def join_sorted(row):
+    if row['issorted']:
+        return "{0}_sorted".format(row['method_freq'])
+    else:
+        return row['method_freq']
+df['method_freq_issorted'] = df.apply(join_sorted, axis=1)
 #%%
 #df['winsize'] = df['winsize'] * pd.Timedelta('15min')
 #%%
@@ -64,28 +82,34 @@ sns.relplot('winsize', 'value', 'calib_freq',
             row='metric', row_order=cols, col='month',
             kind='line', facet_kws=dict(sharex=True, sharey=False),
             data=df)
-
 #%%
-def plot_by_unc(data):
-    cols_by_unc = ['{0}_by_unc'.format(c)  for c in ['mean_crps', 'reliability', 'resolution']]
-    g = sns.relplot('winsize', 'value', 'calib_freq', 
+sns.relplot('winsize', 'value', 'method_freq_issorted', 
+            row='metric', row_order=['mean_crps', 'mae'], col='month',
+            kind='line', facet_kws=dict(sharex=True, sharey=False),
+            data=df)
+#%%
+def plot_by_var(data, var='unc'):
+    cols_by_unc = ['{0}_by_{1}'.format(c, var)  for c in ['mean_crps', 'reliability', 'resolution']]
+    g = sns.relplot('winsize', 'value', 'method_freq', 
                     row='metric', row_order=cols_by_unc, col='month',
                     kind='line', facet_kws=dict(sharex=True, sharey=False),
                     data=data)
     return g
 #%% unsorted VD naive comparison
-g = plot_by_unc(df[~df['issorted']])
+g = plot_by_var(df[~df['issorted']], 'unc')
+#%% 
+g = plot_by_var(df[~df['issorted']], 'mae')
 #%%
-g = plot_by_unc(df[(df['winsize'] <= 24) & (~df['issorted'])])
+g = plot_by_var(df[(df['winsize'] <= 24) & (~df['issorted'])], 'unc')
 ax_squeezed = np.reshape(g.axes, 6)
 ylims = [(0.32, 0.42), (0.16, 0.26), (0, 0.1), (0, 0.1), (0.6, 0.7), (0.79, 0.89)]
 for ax, ylim in zip(ax_squeezed, ylims):
     ax.set_ylim(ylim)
-#%% sorted vs. unsorted
+#%% sorted uncalib vs. unsorted calib!
 # reliability is improved, but resolution is lost due to calibration
 fil = df['month'] == '2018-01'
 cols_by_unc = ['{0}_by_unc'.format(c)  for c in ['mean_crps', 'reliability', 'resolution']]
-g = sns.relplot('winsize', 'value', 'calib_freq', 
+g = sns.relplot('winsize', 'value', 'method_freq', 
             row='metric', row_order=cols_by_unc, col='issorted',
             kind='line', facet_kws=dict(sharex=True, sharey=False),
             data=df[fil])
@@ -96,16 +120,28 @@ for ax, ylim in zip(ax_squeezed, ylims):
     ax.grid(True)
 #%%
 fil = df['month'] == '2018-01'
-fil &= df['calib_freq'] == '15min'
+#fil &= df['calib_freq'] == '15min'
 cols_by_unc = ['{0}_by_unc'.format(c)  for c in ['mean_crps', 'reliability', 'resolution']]
-g = sns.relplot('winsize', 'value', 'issorted', 
-            row='metric', row_order=cols_by_unc,
+g = sns.relplot('winsize', 'value', 'method_freq_issorted', 
+            row='metric', row_order=cols_by_unc, 
             kind='line', facet_kws=dict(sharex=True, sharey=False),
             data=df[fil])
 ax_squeezed = np.reshape(g.axes, 3)
-ylims = [(0.3, 0.8), (0, 0.5), (0.3, 0.8)]
+ylims = [(0.3, 0.8), (0., 0.4), (0.3, 0.8)]
 for ax, ylim in zip(ax_squeezed, ylims):
     ax.set_ylim(ylim)
+#%% Same with by_MAE
+fil = df['month'] == '2018-01'
+#fil &= df['calib_freq'] == '15min'
+cols_by_unc = ['{0}_by_mae'.format(c)  for c in ['mean_crps', 'reliability', 'resolution']]
+g = sns.relplot('winsize', 'value', 'method_freq_issorted', 
+            row='metric', row_order=cols_by_unc, 
+            kind='line', facet_kws=dict(sharex=True, sharey=False),
+            data=df[fil])
+ax_squeezed = np.reshape(g.axes, 3)
+ylims = [(0.3, 0.8), (0., 0.4), (0.3, 0.8)]
+for ax, ylim in zip(ax_squeezed, ylims):
+    pass#ax.set_ylim(ylim)
 #%%
 #fig, axs = plt.subplots(2, 2, sharex=True)
 #for col, ax in zip(cols, np.reshape(axs, (4))):
